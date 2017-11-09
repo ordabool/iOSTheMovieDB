f11990ea13bfd2207c19ffaa1d8fa03aec000ebb
Or <or.dabool@gmail.com>
2017-11-09 17:59:18 +0200

    Initial Commit

---
+++ TheMovieProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMovieProject
+//
+//  Created by Or Dabool on 911//17.
+//  Copyright © 2017 Or Dabool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
